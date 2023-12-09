62e1cb9c35e2ad020415d1c9eb9212170da34eaa
namansaroha02 <92263802+namansaroha02@users.noreply.github.com>
2023-12-09 23:25:36 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSliders, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import Status from "../Status/Status";
@@ -7,58 +7,71 @@ import ByNames from "../ByNames/ByNames";
 import "./Header.css";
 
 function Header() {
-	const [selected, setSelected] = useState('Status');
-	const [secondSelected,secondSetSelected]=useState('Priority');
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // Load initial values from localStorage or use default values
+  const initialSelected = localStorage.getItem('selected') || 'Status';
+  const initialSecondSelected = localStorage.getItem('secondSelected') || 'Priority';
 
-	const handleChange = (e) => {
-		setSelected(e.target.value);
-		setIsDropdownOpen(false);
-	}
-	
-	const secondHandleChange=(e)=>{
-		secondSetSelected(e.target.value);
-		setIsDropdownOpen(false);
-	}
+  const [selected, setSelected] = useState(initialSelected);
+  const [secondSelected, secondSetSelected] = useState(initialSecondSelected);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-	const handleDropdownClick = () => {
-		setIsDropdownOpen(!isDropdownOpen);
-	}
+  // Save selections to localStorage whenever they change
+  useEffect(() => {
+    localStorage.setItem('selected', selected);
+  }, [selected]);
 
-	return (
-		<div className="main">
-			<button
-				className={`dropdownButton ${isDropdownOpen ? 'open' : ''}`}
-				onClick={handleDropdownClick}
-			>
-				<FontAwesomeIcon className="HeaderIconFirst" icon={faSliders} /> Display
-				<FontAwesomeIcon className="HeaderIconSecond" icon={faAngleDown} />
-			</button>
-			<div className={`HeaderGrouping ${isDropdownOpen ? 'visible' : ''}`}>
-				<div>
-					<p className="firstOption">Grouping</p>
-					<select value={selected} onChange={(e) => handleChange(e)}>
-						<option>Status</option>
-						<option>User</option>
-						<option>Priority</option>
-					</select>
-				</div>
-				<div className="secondOption">
-					<p className="firstOption">Ordering</p>
-					<select className="SecondOptionBar" value={secondSelected} onChange={(e) => secondHandleChange(e)}>
-						<option>Priority</option>
-						<option>Title</option>
-					</select>
-				</div>
-			</div>
-			{selected === 'Status' && secondSelected=='Priority' && <Status sorting={"PriorityWise"} />}
-			{selected === 'Status' && secondSelected=='Title' && <Status sorting={"TitleWise"} />}
-			{selected === 'Priority' && secondSelected=='Priority' && <Priority sorting={'PriorityWise'} />}
-			{selected === 'Priority' && secondSelected=='Title' && <Priority sorting={'TitleWise'} />}
-			{selected === 'User' && secondSelected=='Priority' && <ByNames sorting={'PriorityWise'} />}
-			{selected === 'User' && secondSelected=='Title' && <ByNames sorting={'TitleWise'} />}
-		</div>
-	);
+  useEffect(() => {
+    localStorage.setItem('secondSelected', secondSelected);
+  }, [secondSelected]);
+
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+    setIsDropdownOpen(false);
+  }
+
+  const secondHandleChange = (e) => {
+    secondSetSelected(e.target.value);
+    setIsDropdownOpen(false);
+  }
+
+  const handleDropdownClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  }
+
+  return (
+    <div className="main">
+      <button
+        className={`dropdownButton ${isDropdownOpen ? 'open' : ''}`}
+        onClick={handleDropdownClick}
+      >
+        <FontAwesomeIcon className="HeaderIconFirst" icon={faSliders} /> Display
+        <FontAwesomeIcon className="HeaderIconSecond" icon={faAngleDown} />
+      </button>
+      <div className={`HeaderGrouping ${isDropdownOpen ? 'visible' : ''}`}>
+        <div>
+          <p className="firstOption">Grouping</p>
+          <select value={selected} onChange={(e) => handleChange(e)}>
+            <option>Status</option>
+            <option>User</option>
+            <option>Priority</option>
+          </select>
+        </div>
+        <div className="secondOption">
+          <p className="firstOption">Ordering</p>
+          <select className="SecondOptionBar" value={secondSelected} onChange={(e) => secondHandleChange(e)}>
+            <option>Priority</option>
+            <option>Title</option>
+          </select>
+        </div>
+      </div>
+      {selected === 'Status' && secondSelected === 'Priority' && <Status sorting={"PriorityWise"} />}
+      {selected === 'Status' && secondSelected === 'Title' && <Status sorting={"TitleWise"} />}
+      {selected === 'Priority' && secondSelected === 'Priority' && <Priority sorting={'PriorityWise'} />}
+      {selected === 'Priority' && secondSelected === 'Title' && <Priority sorting={'TitleWise'} />}
+      {selected === 'User' && secondSelected === 'Priority' && <ByNames sorting={'PriorityWise'} />}
+      {selected === 'User' && secondSelected === 'Title' && <ByNames sorting={'TitleWise'} />}
+    </div>
+  );
 }
 
 export default Header;
